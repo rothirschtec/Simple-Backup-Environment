207816236eede0693ee67faa3e8e489810c0a429
René Zingerle <rene@rothirsch.tech>
2022-02-03 18:26:55 +0100

    Optimized backup_settings.sh

--- SBE_data/tools/backup_settings.sh
+++ SBE_data/tools/backup_settings.sh
@@ -6,6 +6,12 @@ cd ..
 hdir="$PWD/"
 
 source ${hdir}config
+
+echo; echo $sbdir
+read -p "The content of this directory will be erased (o): " a
+[[ $a == "o" ]] || exit 1
+
+rm -rf ${sbdir}*
 mkdir -p $sbdir
 
 for x in ${hdir}*; do
