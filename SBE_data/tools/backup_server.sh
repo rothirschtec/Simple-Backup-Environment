5c901d1179f95df9c758cf4255ce6936c3a69dc3
René Zingerle - CISSP,SSCP <rene@rothirsch.tech>
2024-06-26 13:32:06 +0200

    Readded queue function

--- SBE_data/tools/backup_server.sh
+++ SBE_data/tools/backup_server.sh
@@ -141,6 +141,115 @@ notify_failure () {
     echo -e "Subject: Backup Error with $sname on $HOSTNAME\n\n $(<${sdir}err.log)" | $sendmail $mail
 }
 
+# Check if there already exists a backup process for the given server and PERIOD
+avoid_duplicates_in_queue () {
+
+  # Create directory if it does not exist
+  [ -d $reports ] || mkdir -p $reports
+
+
+  # Check if the PID used in queue file is active (Delete old entries)
+  for fqueue in ${reports}SBE-queue ${reports}SBE-queue-run
+  do
+    if [ -f ${fqueue} ]; then
+      while read rline
+      do
+        runq=$(awk -F";" '{print $1}' <<< $rline)
+        if [ ! -e /proc/${runq} -a /proc/${runq}/exe ]; then
+          sed -i "/^$runq;.*$/d" ${fqueue}
+        fi
+      done < ${fqueue}
+      sed -i '/^$/d' ${fqueue}
+    else
+      touch ${fqueue}
+    fi
+  done
+
+  # Check queue
+  if cat ${reports}SBE-queue | grep ${sname} | grep ${PERIOD} &> /dev/null; then
+
+    return 1
+
+  else
+
+    # Add the remote server to the queue if the PID does not exist
+    if [ ! -f ${reports}SBE-queue ]; then
+      echo "$$; ${START_DATE}; ${sname}; ${PERIOD};" >> ${reports}SBE-queue
+    else
+      if ! cat ${reports}SBE-queue | grep $$ &> /dev/null; then
+        echo "$$; ${START_DATE}; ${sname}; ${PERIOD};" >> ${reports}SBE-queue
+      fi
+    fi
+
+    return 0
+
+  fi
+
+
+}
+
+
+manage_queue () {
+
+  # Manage queue - To avoid heavy loads
+    st=$(($stmax+1))
+    sti=1
+
+  while [ "$st" -ge "$stmax" ]
+    do
+
+    # Check if SBE-queue-run exists. If not, backup should start immediately
+    if [ -f ${reports}SBE-queue-run ]; then
+
+      #  Delete empty lines in queue files
+      sed -i '/^$/d' ${reports}SBE-queue-run
+
+      # Pick next in queue
+      queue=$(sed -n ${sti}p ${reports}SBE-queue);
+
+      # Check if there's already a backup process for the remote server in queue-run. If so, use next entry in queue
+      if ! cat ${reports}SBE-queue-run | grep ${sname} &> /dev/null; then
+
+        # End loop if queue pid exists and queue run count is less then stmax
+        if [[ $queue =~ "$$;" ]]; then
+          st=$(cat ${reports}SBE-queue-run | wc -l)
+        fi
+
+        # Wait for 2 seconds if
+        if [ $st -ge $stmax ]; then
+          sleep 2
+        fi
+
+        sti=1
+
+      else
+
+        # Increment queue id
+        if [ $(cat ${reports}SBE-queue | wc -l) -gt 1 ]; then
+          (( sti++ ))
+        fi
+
+      fi
+
+    else
+          st=$(($stmax-1))
+    fi
+
+    done
+
+}
+
+
+write_to_queue () {
+
+  cID=$$
+  echo "$cID; ${START_DATE}; ${sname};" >> ${reports}SBE-queue-run
+  sed -i "/^$cID;.*$/d" ${reports}SBE-queue
+
+}
+
+
+
 # MAIN
 
 if [[ "$@" =~ "--set-code" ]]; then
@@ -155,9 +264,16 @@ elif [[ "$@" =~ "--sshCopy" ]]; then
 
 elif [ "$BACKUP" -eq 1 ]; then
     rm -f "${sdir}err.log" "${sdir}bac.log"
-    
+
+    avoid_duplicates_in_queue || exit 2 && [[ "$@" =~ "--log" ]] && echo "Backup added to queue"
+
+    manage_queue && [[ "$@" =~ "--log" ]] && echo "Managed queue"
+
+
     (
         echo "Starting Backup: $(date +"%y-%m-%d %H:%M")"
+
+        write_to_queue && [[ "$@" =~ "--log" ]] && echo "Added backup to queue"
         
         remote_server_up || { echo "Server is down"; exit 1; }
 
